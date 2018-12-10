835da28700d585fad5ce132e0e02ef6abc666edc
petra <petra2222@gmail.com>
2018-12-10 13:44:27 +0100

    Feature engineering part2

--- WifiGit.R
+++ WifiGit.R
@@ -42,7 +42,7 @@ DataTraining[,WAPS] <- sapply(DataTraining[,WAPS], function(x) ifelse(x==100,-12
 # DataTraining[DataTraining==100]<- -120
 
 
-# Teatment of - Near Zero Variance WAPS
+# Treatment of - Near Zero Variance WAPS
 WAPS_NZV_DT <- nearZeroVar (DataTraining[,1:520], saveMetrics=TRUE)
 
 DataTraining <- DataTraining [-which(WAPS_NZV_DT$zeroVar==TRUE)]   # 529 -> 475 variables
@@ -55,7 +55,7 @@ DataTraining1 <- DataTraining[-as.numeric(which(apply(DataTraining[,1:465], 1, v
 
 #------------------ Feature engineering -------------------------
 
-# New variable - HighestWAP
+# New variable - HighestWAP (will be used in Building prediction)
 
 DataTraining1 <- DataTraining1 %>% mutate (HighestWAP=NA)
 
@@ -64,6 +64,36 @@ WAPS <- grep("WAP", names(DataTraining1), value=T)
 DataTraining1 <- DataTraining1 %>% mutate (HighestWAP=colnames(DataTraining1[WAPS]) [apply(DataTraining1[WAPS],1,which.max)])
 
 DataTraining1$HighestWAP <- as.factor(DataTraining1$HighestWAP)
+
+
+
+# not part of feature engineering, but still important (will be used in Building prediction)
+# EACH building has a special WAPS, which are not present in other 2 buildings
+
+# Buildings 1,2,3
+Building1 <- filter(DataTraining1, BUILDINGID == 0) # 5245 obs.
+Building2 <- filter(DataTraining1, BUILDINGID == 1) # 4904 obs. 
+Building3 <- filter(DataTraining1, BUILDINGID == 2) # 9078 obs.
+
+# used WAPS in Building 1
+B1WAPS <- Building1 [,c(1:465)]
+DELB1 <- apply (B1WAPS, 2, function(x) length(unique(x))==1) 
+    # 2 indicates columns
+    # length(unique(Building1$WAP010) returns the total number of "unique" values
+B1WAPS <- B1WAPS [,-c(which(DELB1==TRUE))] # change from 476 to 200 variables
+
+# used WAPS in Building 2
+B2WAPS <- Building2 [,c(1:465)]
+DELB2 <- apply (B2WAPS, 2, function(x) length(unique(x))==1)
+B2WAPS <- B2WAPS [,-c(which(DELB2==TRUE))] # change from 476 to 207 variables
+
+# used WAPS in Building 3
+B3WAPS <- Building3 [,c(1:465)]
+DELB3 <- apply (B3WAPS, 2, function(x) length(unique(x))==1)
+B3WAPS <- B3WAPS [,-c(which(DELB3==TRUE))] # change from 476 to 203 variables
+
+
+
 
 
 
